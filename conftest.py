e251315fb60c98d43521c88273a2751c310c4228
let4eg-hp <g-force@inbox.ru>
2022-11-23 23:01:52 +0300

    Обновлен файл

--- conftest.py
+++ conftest.py
@@ -4,3 +4,10 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+@pytest.fixture()
+def set_webdriver():
+    browser.config.driver = webdriver.Chrome(ChromeDriverManager().install())
+    return browser.config.driver
+
+@pytest.fixture()
+def open_browser():
--- test_search.py
+++ test_search.py
@@ -1,6 +1,14 @@
 from selene.support.shared import browser
 from selene import be, have
 
+#Доработайте тест с первого занятия с Pytest
+
+#Выставите размер браузера с помощью фикстуры Pytest
+
+#Добавьте еще один "негативный" тест на поиск, чтобы результат поиска не выдавал результатов
+
+#Запушьте код в свой репозиторий и дайте на него ссылку в качестве ответа на домашнее задание
+
 def test_positive_search(open_browser):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
