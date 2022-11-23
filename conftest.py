434d48e567f763fea9e4cd5497ac686def686654
let4eg-hp <g-force@inbox.ru>
2022-11-23 22:37:34 +0300

    Обновлен файл

---
+++ conftest.py
@@ -0,0 +1,6 @@
+import pytest
+from selene.support.shared import browser
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
--- test_search.py
+++ test_search.py
@@ -1,6 +1,13 @@
 from selene.support.shared import browser
 from selene import be, have
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+def test_positive_search(open_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_negative_search(open_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('abrakadabra').press_enter()
+    browser.element('[id="trollolo"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
