6965da840c4d722ddedba41e40d5773ce3695b12
let4eg-hp <g-force@inbox.ru>
2022-11-25 21:28:34 +0300

    Пуш для  задания 2 занятия

--- test_search.py
+++ test_search.py
@@ -10,12 +10,8 @@ from selene import be, have
 #Запушьте код в свой репозиторий и дайте на него ссылку в качестве ответа на домашнее задание
 
 def test_positive_search(open_browser):
-    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('макаки').press_enter()
     browser.element('[id="search"]').should(have.text('макаки'))
-
-
 def test_negative_search(open_browser):
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('макака').press_enter()
-    browser.element('[id="search"]').should(have.text('горилла'))
+    browser.element('[name="q"]').should(be.blank).type('41212sdfasda').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('По запросу 41212sdfasda ничего не найдено. '))
